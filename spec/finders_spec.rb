24d67207f236bbf8ccc3556ad9eefa3394cf080f
adrianpike <adrian.pike@gmail.com>
2010-11-05 14:49:29 -0700

    fix the unimplemented specs

--- spec/finders_spec.rb
+++ spec/finders_spec.rb
@@ -10,21 +10,16 @@ describe 'Hyperions finders' do
 		
 		f2 = DefaultKey.find2('test')
 		f2.content.should == f.content
-		
-		f3 = DefaultKey.find2(:all, :conditions => {:key => "test"})
-		f3.content.should == f.content
-		
-		f4 = DefaultKey.find2(:first, :conditions => {:key => 'test', :content => string1})
-		f4.content.should == f.content
+	end	
 	
-		f5 = DefaultKey.find2(:first, :conditions => {:key => {:min => 'a', :max => 'z'}}, :limit => 10, :order => content)
-	end
-
 	it 'should be able to find all items exact matching an index'
+	# DefaultKey.find2(:all, :conditions => {:key => "test"})
 
 	it 'should be able to find all items range matching an index'
+	# DefaultKey.find2(:first, :conditions => {:key => {:min => 'a', :max => 'z'}}, :limit => 10, :order => content)
 
 	it 'should be able to find all items exact matching multiple indexes'
+	# DefaultKey.find2(:first, :conditions => {:key => 'test', :content => string1})
 
 	it 'should be able to find all items range matching multiple indexes'
 	
